2ba857c78079f885518feacd96a647bb7b00ecc2
gnuga1 <115860949+gnuga1@users.noreply.github.com>
2022-10-30 21:30:55 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,34 @@
+package encapsulation;
+
+/**
+ *
+ * @author DELL agung Gumelar
+ */
+public class Person {
+    private final String name;
+    private String address;
+   
+   public Person(String name, String address) {
+      this.name = name;
+      this.address = address;
+   }
+   
+   public String getName() {
+      return name;
+   }
+  
+   
+   public String getAddress() {
+      return address;
+   }
+   
+   public void setAddress(String address) {
+      this.address = address;
+   }
+   
+   @Override
+   public String toString() {
+      return name + "(" + address + ")";
+   }
+}
+
